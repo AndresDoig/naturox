b0c3e5f4ba463c619070d19767638c860aa80b2d
AndresDoig0711 <andres.doig92@gmail.com>
2023-12-04 19:55:28 -0500

    updated logo

--- js/index.js
+++ js/index.js
@@ -13,19 +13,4 @@ navLinks.forEach(link => {
     })
 })
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -5.181540886283448, lng: -80.63434535670615 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 18,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
   
